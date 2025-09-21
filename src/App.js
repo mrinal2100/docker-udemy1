1c3bc6fd097e74e8f7ad60b125239736cbee1d56
mrinal2100 <mrinals2100@gmail.com>
2025-09-21 12:30:03 -0400

    made changes to src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
            Edit <code>src/App.js</code> and save to reload.
-	   Hello Mrinal how is USA
+	   Hello Mrinal how is weather in USA
         </p>
         <a
           className="App-link"
